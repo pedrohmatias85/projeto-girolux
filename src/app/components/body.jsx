7f137e8ce9c9ea7c998cbc588d1377ac46784123
pedro <pedrohmatias85@gmail.com>
2023-12-29 11:16:47 -0400

    terminado 3 telas do desktop

--- src/app/components/body.jsx
+++ src/app/components/body.jsx
@@ -5,6 +5,9 @@ import AtendimentoIcon from "../assets/atendimento 1.svg"
 import IconeEstrela from "../assets/iconeestrela.png"
 import IconeGoogle from "../assets/icone do Google.png"
 import IconeEmpresas from "../assets/Logos completos mobile.png"
+import BackgroundDesktop from '../assets/backgrounddesktop.png'
+import IMGInfraestrutura from '../assets/infraestrutura .png'
+import IMGtomadas from '../assets/tomadaseinterruptores.png'
 
 
 
@@ -16,68 +19,65 @@ import Botao from "./contactbutton"
 
 export default function body () {
     return (
-
+            
         <div className="text-center">
-            <div className="text-center justify-center">
-                <Image className='inline md:bg-cover' src={BgImage} alt={'background'}  ></Image>
+
+            {/*  Início da primeira tela */}
+            <div className="text-center justify-center xl:max-h-[60vh] md:max-h-[45vh]">
+                <Image className='inline md:hidden' src={BgImage} alt={'background'}  ></Image>
+                <Image className=' hidden md:block' src={BackgroundDesktop}></Image>
                 <Botao></Botao>
             </div>
-
-            <div className="md:flex justify-center gap-10">
-            <div className="text-center md text-gray font-extrabold text-lg md:text-2xl md:flex-col md:first:mt-36 ">
-                <p>Está a procura de <span className="text-primary-orange font-extrabold"> materiais elétricos</span></p>
+            <div className="md:flex justify-center xl:gap-60 ">
+            <div className="text-center md:text-left md:text-4xl text-gray md:text-primary-orange font-extrabold text-lg md:text-2xl md:flex-col  md:mt-20 xl:mt-36 md:pl-10 ">
+                <p>Está a procura de <span className="text-primary-orange font-extrabold md:bg-gray md:rounded-xl md:px-1"> materiais elétricos</span></p>
                 <p>para todas as fases da sua obra?</p>
                 <div className="bg-primary-orange ml-40 w-14 h-1.5 rounded-full mt-2 md:hidden "></div>
-                <p className="font-extrabold mb-10 text-xl mt-10 md:text-center">Prazer, somos a girolux!</p>
-                
+                <p className="font-extrabold text-xl mt-10 md:text-left md:text-gray md:font-normal md:text-2xl">Prazer, somos a <span className="md:font-bold">girolux!</span></p> 
+                <div className="bg-gray w-14 h-1 rounded-full hidden md:block"></div>
             </div>
-            <div className="text-center md:h-[50vh] bg-primary-orange md:bg-white mt-10 py-10 text-gray md:ml-5">
-                
+            <div className="text-center md:h-[50vh] bg-primary-orange md:bg-white mt-10 py-10 text-gray md:mr-10"> 
                 <div className=' md:flex gap-5'>
-
                     <div className="bg-primary-orange flex md:flex-col justify-center text-center rounded-2xl md:h-[30vh] md:w-[34vh]">
-                    <div className="md:pl-28 pr-3"><Image className="mt-3" src={AtendimentoIcon} alt="icone-atendimento"></Image></div>
+                    <div className="md:pl-32 pr-3 pb-5"><Image className="mt-3" src={AtendimentoIcon} alt="icone-atendimento"></Image></div>
                     <div className="text-left md:text-center mt-1">
                         <p className="font-bold">Atendimento de excelência</p>
                         <p className="text-white">Profissionais capacitados com </p>
                         <p className="text-white">anos de experiência no mercado.</p>
                     </div>
                     </div>
-
-
                 <div className=" bg-primary-orange flex md:flex-col justify-center text-center rounded-2xl md:h-[30vh] md:w-[34vh] mt-5 md:mt-0">     
-                <div className="md:ml-32 mt-3 -ml-5"><Image  className="mt-1" src={SeloQualidade} alt='icone-qualidade'></Image></div>
+                <div className="md:ml-36 mt-3 -ml-5 pb-5"><Image  className="mt-1" src={SeloQualidade} alt='icone-qualidade'></Image></div>
                         <div className="text-left md:text-center mt-1 pl-6">
                             <p className="font-bold"> Produtos de Qualidade</p>
                             <p className="text-white">Encontre marcas renomadas</p>
                             <p className="text-white"> e garantia de fabricação</p>
                         </div>
                 </div>
-                
-            
-            
-            
-            
             </div> 
-            
             </div> 
             </div>
-            <div className="mt-5 ml-5 flex">
-                <div className="bg-primary-orange w-1.5 h-14 rounded-full mt-2 "></div>
-                <div className=""><h1 className="font-extrabold text-gray mt-2 ml-2 text-xl text-left">Está construindo ou reformando?</h1>
-                    <p className=" text-primary-orange font-bold text-left ml-2">Nosso compromisso é com seu lar único</p>
+            {/*  Final da primeira tela */}
+
+            <div className="mt-2 flex justify-center">
+                <div className="bg-primary-orange w-1.5 h-14 rounded-full mt-2 md:hidden "></div>
+                <div className="text-left md:text-center ml-2 ">
+                    <h1 className="font-extrabold text-gray mt-2 text-xl md:font-black md:text-4xl">Está construindo ou reformando?</h1>
+                    <p className=" text-primary-orange font-bold md:font-black md:text-2xl ">Nosso compromisso é com seu lar único</p>
                 </div>
             </div>
             
-            <div className="mt-5">
-                <div className="text-left h-[50vh] bg-[url('/imgs/background2.png')] bg-cover bg-no-repeat bg-">
-                    <div className="pt-10 pl-5 text-white">
-                        <p className="font-extrabold text-3xl">Tudo em</p>
-                        <p className="font-extrabold text-3xl">infraestrutura!</p>
+             {/*  Início da segunda tela */}
+
+            <div className="mt-5 bg-primary-orange h-[50vh] gap-60 flex justify-center md:justify-between">
+                <div className="text-left md:ml-20">
+                    <div className="pt-10 pl-5 text-white font-extrabold text-3xl xl:text-6xl">
+                        <p>Tudo em</p>
+                        <p>infraestrutura!</p>
                     </div>
-                    <div className="ml-5 mt-3 text-gray flex font-medium">
+                    <div className="ml-5 mt-10 text-gray flex font-medium xl:text-2xl md:gap-56">
                         <div>
-                            <div className="mt-3">
+                            <div className="mt-4">
                                 <p>Tubulação</p>
                                 <div className="bg-gray w-10 h-1 rounded-full mt-3"></div>
                             </div>
@@ -85,40 +85,53 @@ export default function body () {
                                 <p>Padrão</p>
                                 <p>de energia</p>
                                 <div className="bg-gray w-10 h-1 rounded-full mt-1"></div>
-                                <div className="mt-5">
-                                <p>Quadros</p>
-                                <p>de distribuição</p>
-                                <div className="bg-gray w-10 h-1 rounded-full mt-1"></div>
-                            </div>
+                                
                             </div>
                         </div>
-                        <div className="ml-1 mt-3">
+                        <div className="ml-5 mt-">
                             <div>
                                 <p>Eletrodutos</p>
                                 <p>Corrugados</p>
                                 <div className="bg-gray w-10 h-1 rounded-full mt-1"></div>
-                            </div>
-                            
-                        </div>
+                                <div className="mt-5">
+                                <p>Quadros</p>
+                                <p>de distribuição</p>
+                                <div className="bg-gray w-10 h-1 rounded-full mt-1"></div>
+                                </div>
+                            </div>       
+                        </div>     
                     </div>
+                </div>
+                <div className="hidden md:flex">
+                <Image src={IMGInfraestrutura} className="mt-4"></Image>
                 </div>
             </div>
 
-                <div className="bg-[url('/imgs/background3.png')] bg-cover bg-no-repeat h-96 bg-left">
-                    <div className="text-right mr-1 pt-10 flex flex-col items-end">
-                        <div className="text-primary-orange pt-12 pr-20 font-black text-xl">
+             {/*  Final da segunda tela */}
+
+             {/* início da terceira tela */}
+                <div className="bg-white h-[60vh] flex justify-between">
+                    <div className="md:mt-20 md:ml-10 scale-y-50 md:scale-100 ">
+                         <Image src={IMGtomadas}></Image>
+                    </div>
+
+                    <div className=" text-left md:text-right md:pr-52 md:pt-24">
+                        <div className="text-primary-orange pt-20 pr-1 font-black md:text-6xl ">
                             <p> Interruptores</p>
-                            <p className="pr-8"> e tomadas</p>
+                            <p className=""> e tomadas</p>
                         </div>
-                         <div  className="text-gray text-sm mr-3 mt-1">
+                         <div  className="text-gray text-sm md:text-2xl mr-3 mt-3">
                             <p className="font-bold">Variedade e linhas exclusivas</p>
-                            <span className="text-start font-medium">
+                            <span className=" font-medium">
                             <p>Para o seu ambiente</p>
                             </span>
-                            <div className="bg-gray w-5 h-0.5 rounded-full"></div>
+                            <div className="bg-gray w-5 h-0.5 rounded-full md:bg-primary-orange md:w-20 md:h-1 md:ml-96 md:mt-5"></div>
                          </div>
                     </div>
+
                 </div>
+
+
                 <div className="bg-[url('/imgs/background5.png')] bg-cover bg-no-repeat h-80 bg-right-bottom">
                     <div>
                         <p className="text-primary-orange font-black text-2xl pt-12 text-left pl-3">Iluminação</p>
@@ -141,7 +154,7 @@ export default function body () {
                     </div>
                 </div>
                 <div className="bg-[url('/imgs/background4.png')] bg-cover h-80 bg-no-repeat bg-right-bottom ">
-                <Botao></Botao>
+                <div className="hidden" ><Botao></Botao></div>
                  <div className="flex flex-col items-end">
                         <div className="text-white text-2xl font-black text-left pr-5 pt-3">
                             <div>
--- src/app/components/contactbutton.jsx
+++ src/app/components/contactbutton.jsx
@@ -1,15 +1,17 @@
 import Link from "next/link"
 import Image from "next/image"
 import WhatsappIcon from '../assets/whatsapp-icon.png'
+import WhatsIconMD from '../assets/Whats.png'
 
 
 export default function ContactButton () {
     return (
-        <form className="relative -top-8">
+        <form className="relative -top-8 md:left-[38vh] md:-top-[24vh] xl:-top-[30vh] xl:left-[50vh]">
             <Link href={'https://wa.me/556730386433'}>
-                <button className="inline-flex font-bold bg-gray text-white w-72 h-16 text-center items-center  px-5 py-5 rounded-full">
-                    <Image src={WhatsappIcon} alt={'icone whatsapp'} className="mr-2"></Image>
-                    <p className="ml-3">Entrar em contato</p>
+                <button className="inline-flex font-bold bg-gray md:bg-primary-orange text-white w-72 h-16 xl:w-96 xl:h-24 text-center items-center  px-5 py-5 rounded-full hover:scale-105">
+                    <Image src={WhatsappIcon} alt={'icone whatsapp'} className="mr-2 md:hidden"></Image>
+                    <Image src={WhatsIconMD} className="hidden md:block" height={58} width={58}></Image>
+                    <p className="ml-3 xl:ml-7 xl:text-2xl">Entrar em contato</p>
                 </button>
             </Link>
         </form>
