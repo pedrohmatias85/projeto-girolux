26516c521a47f359644a9e8056d96625eb88af1c
pedro <pedrohmatias85@gmail.com>
2024-01-04 15:24:42 -0400

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Footer from './components/footer'
 export default function Home() {
   return (
     <div className='mx-auto'>
+
      <Body></Body>  
      <Footer></Footer>               
     </div>
